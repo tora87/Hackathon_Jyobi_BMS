e3fdc0499a52bad8271b52e0412c13511d6ea2c1
arihara ryunosuke <r.arihara.sys20@morijyobi.ac.jp>
2021-09-02 12:18:57 +0900

    [Add] QRコードを添付したメールの送信機能を作成

--- create_qr.py
+++ create_qr.py
@@ -1,12 +1,16 @@
 from flask import Flask, Blueprint, render_template
 import qrcode
 import os
+from smtplib import SMTP
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+
 
 # 有原 担当
 qr = Blueprint('create_qr', __name__)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "\\Hackathon_Jyobi_BMS\\"
-print(BASE_DIR)
 
 
 @qr.route('/')
@@ -24,21 +28,86 @@ def make_qr(request):
 class makeQR:
     """
     任意のデータを含んだQRコードを生成する
-    generated_qr_code()に、任意の数の引数(文字列)を与えると、static/images/ に
-    qrcode.pngが生成される
     """
     def __init__(self):
         self.output = BASE_DIR + '/static/images/qrcode.png'
         self.qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_Q, box_size=10, border=4, )
 
-    def generate_qr_code(self, *args: 'student_number, student_name') -> 'qr_images.png':
+    def generate_qr_code(self, **kwargs: 'student_id, student_name') -> 'qr_images.png':
+        """
+        受け取った引数を、QRコードとして生成する。static/images/qrcode.pngとして保存される
+
+        Parameters
+        ----------
+        kwargs['student_id'] : integer
+            対象の生徒の学籍番号。
+        kwargs['student_name'] : string
+            対象の生徒の名前。
+        """
         text = ""
-        self.qr.add_data([text + data for data in args])
+        self.qr.add_data(kwargs['student_id'] + '+' + kwargs['student_name'])
         self.qr.make(fit=True)
         img = self.qr.make_image(fill_color="black", back_color="white")
         img.save(self.output)
 
 
+class Email:
+    """
+    QRコードを添付したhtmlメールを送信する
+    """
+    def __init__(self, **kwargs):
+        """
+        コンストラクタ
+
+        Parameters
+        ----------
+        kwargs['Subject'] : String
+            送信するメールの題(タイトル)。
+        kwargs['body'] : string
+            送信するメールの本題部分。
+        kwargs['id'] : string(email)
+            送信元のメールアドレス
+        kwargs['password'] : password
+            送信元のメールアドレスのパスワード
+        """
+        self.subject = kwargs['Subject']
+        self.body = kwargs['body']
+        self.ID = kwargs['id']  # 固定のアドレスに変更も可
+        self.PASS = kwargs['password']
+        self.HOST = "smtp.gmail.com"
+        self.PORT = 587
+
+    def send_email(self, sender_email_address: 'recipient email_address'):
+        """
+        受け取った引数のメールアドレスに宛てて、QRコードを添付したメールを送信する。
+
+        Parameters
+        ----------
+        sender_email_address : strnig(email)
+            送信先のメールアドレス
+        """
+        msg = MIMEMultipart()
+        self.body.replace("\n", "<br>")
+        
+        msg.attach(MIMEText(self.body, "html"))
+        img_data = open(BASE_DIR+'\\static\\images\\qrcode.png', 'rb')
+        msg.attach(MIMEImage(img_data.read()))
+
+        msg["Subject"] = self.subject
+        msg["From"] = self.ID
+        msg["To"] = sender_email_address
+        server = SMTP(self.HOST, self.PORT)
+        server.starttls()
+
+        server.login(self.ID, self.PASS)
+
+        server.send_message(msg)
+
+        server.quit()
+
+
 if __name__ == '__main__':
-    app = makeQR()
-    app.generate_qr_code("4204101", "有原隆乃介")
+    text_subject = "テスト送信"
+    text_body = "BMSシステム内の、メール送信機能のテストです。\nQRコードが添付されていれば成功です。"
+    email = Email(Subject=text_subject, body=text_body, id="", password="")  # 送信元のメールアドレス、パスワードを記入
+    email.send_email("")  # 送信先のメールアドレスを記入
