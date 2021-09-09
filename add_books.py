ad2ce043d7cb713c3d4cca836be650e226287419
kodama ryosuke <r.kodama.sys20@morijyobi.ac.jp>
2021-09-09 13:51:23 +0900

    add_booksにsession比較処理追加

--- add_books.py
+++ add_books.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, render_template, request
+from flask import Flask, Blueprint, render_template, request, session, redirect
 from flask.wrappers import Request
 from databases.getbooklist import select_bookdetail
 from databases.addbookdetail import adbook_detail
@@ -8,8 +8,8 @@ books = Blueprint('add_books', __name__, url_prefix='/add_books')
 # url http://127.0.0.1:5000/add-books/
 @books.route('/', methods=['GET' ,'POST'])
 def add_books():
-    # if "user" not in session: #? セッションの有無
-    #     return redirect("/")
+    if "user_id" not in session: #? セッションの有無
+        return redirect("/")
     if request.method == "GET":
         return render_template('add_books.html')
     else:
