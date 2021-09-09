6ac4458a44b37bf920dec5d96cddb22566dd4344
arihara ryunosuke <r.arihara.sys20@morijyobi.ac.jp>
2021-09-09 11:28:05 +0900

    [Add] ログイン処理を作成しました。 ログインページから学籍番号、名前を受け取り、DBの登録情報と照合して、一致しなければログインページを返し、一致すればsessionに学籍番号を保持してhome.htmlを返します。

---
+++ databases/login_db.py
@@ -0,0 +1,46 @@
+import MySQLdb
+
+
+def select_specify_user(user_id: 'int'):
+    """
+    学籍番号を受け取り、DBから名前を取得する。
+
+    Parameters
+    ----------
+    user_id : integer
+        検索する学籍番号
+
+    Returns
+    -------
+    data : tuple or None
+        学籍番号と紐づいた名前
+    """
+
+    conn = get_connection()
+    cur = conn.cursor()
+
+    sql = 'select name from user where number = %s'
+
+    try:
+        cur.execute(sql, (user_id,))
+    except Exception as e:
+        print('SQL実行に失敗しました:', e)
+
+    user = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    return user
+
+
+def get_connection():
+    return MySQLdb.connect(user='hackathon-2', passwd='hackathon', host='localhost', db='jyobi_bms', charset='utf8')
+
+
+if __name__ == '__main__':
+    user_name = select_specify_user(user_id=0000000)
+    if user_name is None:
+        print('this is None type')
+    else:
+        print('this is not None type')
