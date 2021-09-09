71892cc78401862242f8baa250a94a14e14de02c
kodama ryosuke <r.kodama.sys20@morijyobi.ac.jp>
2021-09-09 12:41:09 +0900

    [Add]図書追加機能

---
+++ databases/addbookdetail.py
@@ -0,0 +1,26 @@
+import MySQLdb
+
+# test用localhost_access
+def connect_db():
+    conn = MySQLdb.connect(
+        host = '127.0.0.1',
+        user = 'root',
+        passwd = 'kodama1381',
+        db = 'jyobi_bms',
+        charset = 'utf8'
+    )
+    return conn
+
+def adbook_detail(jancord,title,author,stock):
+    try:
+        conn = connect_db()
+        cur = conn.cursor()
+        sql = "INSERT INTO books VALUES(%s,%s,%s,%s)"
+        cur.execute(sql,(jancord,title,author,stock,))
+        cur.close()
+        conn.commit()
+        conn.close()
+        return True
+    except Exception as e:
+        print(e)
+    return False
