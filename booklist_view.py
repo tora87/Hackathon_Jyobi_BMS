9c1d4bb2a95dcc8f0bb332ca2440ba3fba1a2dfa
kodama ryosuke <r.kodama.sys20@morijyobi.ac.jp>
2021-09-09 13:46:16 +0900

    [change]booklist_viewにsession比較処理追加

--- booklist_view.py
+++ booklist_view.py
@@ -7,16 +7,16 @@ bol_view = Blueprint('booklist_view', __name__)
 #! 図書一覧===============================
 @bol_view.route('/booklist')
 def booklist_view():
-    # if "user" not in session: #? セッションの有無
-    #     return redirect("/")
+    if "user_id" not in session: #? セッションの有無
+        return redirect("/")
     books_all_table = select_allbooks() #? 図書テーブルから全図書の取得[JANcode,book_name]
     return render_template('books_list.html', books_all_table=books_all_table)
 
 #! JANコードから詳細表示==================
 @bol_view.route('/booklist/<int:jancord>', methods=['GET','POST'])
 def detail_view(jancord):
-    # if "user" not in session: #? セッションの有無
-    #     return redirect("/")
+    if "user_id" not in session: #? セッションの有無
+        return redirect("/")
     books_all_table = select_allbooks() #? 図書テーブルから全図書の取得[JANcode,book_name]
     sel_book_detail = select_bookdetail(jancord) #? 図書のデータの取得[JANcode,book_name,book_author,AllAmo,remainingAmo]
     return render_template('books_list.html', books_all_table=books_all_table, sel_book_detail=sel_book_detail)
@@ -24,16 +24,16 @@ def detail_view(jancord):
 #! 詳細編集ページ表示==================
 @bol_view.route('/booklist/<int:jancord>/edit')
 def edit_view(jancord):
-    # if "user" not in session: #? セッションの有無
-    #     return redirect("/")
+    if "user_id" not in session: #? セッションの有無
+        return redirect("/")
     sel_book_detail = select_bookdetail(jancord)
     return render_template('edit.html', sel_book_detail=sel_book_detail, jancord=jancord)
 
 #! 詳細の更新==================
 @bol_view.route('/booklist/<int:jancord>/update_detail', methods=['POST'])
 def update_detail(jancord):
-    # if "user" not in session: #? セッションの有無
-    #     return redirect("/")
+    if "user_id" not in session: #? セッションの有無
+        return redirect("/")
     title = request.form.get("name")
     author = request.form.get("author")
     stock = request.form.get("stock")
@@ -46,8 +46,8 @@ def update_detail(jancord):
 #! 本の削除==================
 @bol_view.route('/booklist/<int:jancord>/delete_detail', methods=['POST'])
 def delete_detail(jancord):
-    # if "user" not in session: #? セッションの有無
-    #     return redirect("/")
+    if "user_id" not in session: #? セッションの有無
+        return redirect("/")
     result = dlt_d(jancord)
     if (result):
         return redirect(url_for('booklist_view.booklist_view'))
