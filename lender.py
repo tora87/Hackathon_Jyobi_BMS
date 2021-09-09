1ff5c84bcd74db4f6daa697097285574c80e3f32
arihara ryunosuke <r.arihara.sys20@morijyobi.ac.jp>
2021-09-09 10:26:53 +0900

    [Add] lender.pyに、貸出時、返却時の処理を作成しました。 フロントからの返り値を成形していない大まかな物です。

--- lender.py
+++ lender.py
@@ -1,8 +1,10 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, session, request, redirect, url_for
+
+import lender
 from databases import lender_db
 
 # 有原 担当
-lend = Blueprint('lender', __name__, url_prefix='/lend-borrow')
+lend = Blueprint('lender', __name__, url_prefix='/lend-return')
 
 
 # url http://127.0.0.1:5000/lend-borrow/
@@ -21,6 +23,46 @@ def get_lender():
         })
     print(book_data)
     return render_template('lender.html', books_json=book_data)
+
+
+# 一つの関数で貸出、返却処理することになるかも
+@lend.route('lend', methods=['POST'])
+def lend_process():
+    # 貸出処理
+    # 受け取ったデータから、本のisbnコードを受け取る
+    # 学籍番号を、sessionから取得する
+    user_id = session['user_id']
+    isbn = request.form.get('isbn')
+
+    # 受け取ったデータをdbに登録する
+    flg = lender_db.insert_specify_book(user_number=user_id, book_number=isbn)
+
+    if flg:
+        print('ok')
+    else:
+        print('couldn\'t lend')
+
+    return redirect(url_for('lend.get_lender'))
+
+
+@lend.route('return', methods=['POST'])
+def return_process():
+    # 返却処理
+    # 受け取ったデータから、本のisbnコードを受け取る
+    # 学籍番号を、sessionから取得する
+    user_id = session['user_id']
+    isbn = request.form.get('isbn')
+    
+    # 受け取ったデータをdbに登録する
+    flg = lender_db.update_return_book(user_number=user_id, book_number=isbn)
+    
+    if flg:
+        print('ok')
+    else:
+        print('couldn\'t return')
+        
+    return redirect(url_for('lend.get_lender'))
+
 
 # @lend.route('/', method='POST')
 # def post_lender():
