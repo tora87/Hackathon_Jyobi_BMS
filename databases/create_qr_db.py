2888b6dc21c635a29b2b4cf86e2e774dbd93a991
kodama ryosuke <r.kodama.sys20@morijyobi.ac.jp>
2021-09-15 11:43:07 +0900

    [Update]学籍番号での検索機能

--- databases/create_qr_db.py
+++ databases/create_qr_db.py
@@ -33,6 +33,38 @@ def select_all_user() -> 'data':
     return user
 
 
+def select_search_user(selectnum):
+    conn = connect_db()
+    cur = conn.cursor()
+
+    sql = "SELECT * FROM user WHERE number BETWEEN %s AND %s"
+    try:
+        cur.execute(sql, (int(selectnum), int(selectnum)+99999))
+    except Exception as e:
+        print('SQL実行に失敗しました:', e)
+        return None
+    user = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    return user
+
+
+def select_number_scope():
+    conn = connect_db()
+    cur = conn.cursor()
+    sql = "SELECT DISTINCT(number DIV 100000 * 100000) FROM user WHERE number > 1"
+    try:
+        cur.execute(sql,)
+    except Exception as e:
+        print('SQL実行に失敗しました:', e)
+        return None
+    number = cur.fetchall()
+    cur.close()
+    conn.close()
+    return number
+
+
 def select_for_generation_user_data(student_id: 'int') -> 'data':
     """
     qrコード生成時に必要なデータの取得
--- static/js/create_qr.js
+++ static/js/create_qr.js
@@ -1,1 +1,18 @@
 console.log("create_qr.js")
+
+$(function() {
+	$("#select_number_form").change( function () {
+		console.log("selected")
+		let sn = $(this).val();
+        $.ajax({
+            type: 'POST',
+            url: '/generate-qr/search',
+            data: {'selectnum': sn},
+        }).done(function (res) {
+            $('body').html(res);
+        }).error(function (err) {
+            console.log(err);
+        })
+    })
+
+})
