72d76e7c5c6fad3e6f1d529e8a9f4a73c05838d0
kentamjb <kenta.sato.sys19@morijyobi.ac.jp>
2021-09-21 09:39:53 +0900

    [apdate]JANコード読み取り時の枠線追加

--- static/js/lender.js
+++ static/js/lender.js
@@ -29,5 +29,34 @@ Quagga.onDetected(success => {
     const code = success.codeResult.code;
     jancode.value = code;
 
-    sendForm.submit();
+    console.log(code);
+
+    // sendForm.submit();
+})
+
+Quagga.onProcessed(data => {
+  const ctx = Quagga.canvas.ctx.overlay;
+  const canvas = Quagga.canvas.dom.overlay;
+
+  if (!data) { return; }
+
+  // 認識したバーコードを囲む
+  if (data.boxes) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    const hasNotRead = box => box !== data.box;
+    data.boxes.filter(hasNotRead).forEach(box => {
+      Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, ctx, { color: 'green', lineWidth: 2 });
+    });
+
+     // 読み取ったバーコードを囲む
+     if (data.box) {
+      Quagga.ImageDebug.drawPath(data.box, { x: 0, y: 1 }, ctx, { color: 'blue', lineWidth: 2 });
+    }
+
+    // 読み取ったバーコードに線を引く
+    if (data.codeResult && data.codeResult.code) {
+      Quagga.ImageDebug.drawPath(data.line, { x: 'x', y: 'y' }, ctx, { color: 'red', lineWidth: 3 });
+    }
+}
 })
