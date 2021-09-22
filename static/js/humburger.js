0480cef192a8e4d0f96441bbb10d47e31b104a42
tora87 <t.hanasaka.sys19@morijyobi.ac.jp>
2021-09-22 12:25:19 +0900

    [change]ヘッダーレスポンシブ化。 ハンバーガーメニューの追加

---
+++ static/js/humburger.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded',() => {
+  const menu_btn = document.querySelector('#menu-btn');
+  const menu_content = document.querySelector('#humburger-menu-content');
+
+  menu_btn.addEventListener('click',() => {
+    if(menu_btn.classList.contains('active')){
+      menu_btn.classList.remove('active');
+      menu_content.classList.remove('active');
+    }else {
+      menu_btn.classList.add('active');
+      menu_content.classList.add('active');
+    }
+  })
+})
