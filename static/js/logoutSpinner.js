dc798bb85496bdf849d6a82de2f68872dc9ea830
sos05o <r.arihara.sys20@morijyobi.ac.jp>
2021-09-24 11:18:22 +0900

    [Fix] ログアウト時のカウントダウン機能を作成しました。

--- static/js/logoutSpinner.js
+++ static/js/logoutSpinner.js
@@ -2,6 +2,8 @@ $(function(){
     $(".log-out-wrapper a").on("click", function(e) {
         e.preventDefault();
         let url = $(this).attr("href");
+        let count = 4;
+        let timer = $("#timer");
         if (url !== "") {
             $(".load-wrapp").show();
 
@@ -9,9 +11,14 @@ $(function(){
                 window.location = url;
             }, 5000);
 
+            let counter = setInterval(function(){
+                timer.text(count--);
+            }, 1000);
+
             $(".load-wrapp").on("click", function() {
                 $(".load-wrapp").hide();
                 clearInterval(time);
+                clearInterval(count);
                 return;
             })
         }
