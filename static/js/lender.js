d155b1fd132a15f70314915f72ce48770aead548
kentamjb <kenta.sato.sys19@morijyobi.ac.jp>
2021-09-24 09:33:05 +0900

    [apdate]コード読み取り時の仕様変更

--- static/js/lender.js
+++ static/js/lender.js
@@ -33,8 +33,6 @@ Quagga.onDetected(success => {
     const code = success.codeResult.code;
     jancode.value = code;
 
-    console.log(`読み取ったコード=> ${code}`)
-
     jancode_list.push(code);
 
     for(let i = 1; i < jancode_list.length; i++){
@@ -43,49 +41,9 @@ Quagga.onDetected(success => {
       }
     }
 
-    if(count >= 40){
-    //       sendForm.submit();
-
+    if(count >= 200){
+      sendForm.submit();
     }
-
-    // jancode_list.push({code:code,count: () => {
-    //   for(let i = 0; i < jancode_list.length; i++){
-    //     if(code == jancode_list[i].code){
-    //       jancode_list[i].count += 1;
-    //     } else {
-    //       jancode_list.push({code:code,count:1});
-    //     }
-    //   }
-    // }})
-
-    // console.log(jancode_list);
-    // for(let i = 0; i < jancode_list.length; i++ ){
-    //   if(code == jancode_list[i]["number"]){
-    //     jancode_list[i]["count"] += 1;
-    //     if(jancode_list[i]["count"] >= 100){
-    //       sendForm.submit();
-    //     } else {
-    //       console.log(jancode_list[i]["count"]);
-    //     }
-    //   } else {
-    //     console.log('check')
-    //     jancode_list.push({number:code,count:1})
-    //     console.log(jancode_list)
-    //   }
-    // }
-    
-    // if(jancode_list.length == 0 ) jancode_list.push({jancode: code,count: 1});
-
-    // if(jancode_list.length > 0){
-    //   for(let i = 0; i < jancode_list.length; i++){
-    //     if(code == jancode_list[i].jancode){
-    //       jancode_list[i].count += 1;
-    //     }else{
-    //       jancode_list.push({jancode:code, count:1})
-    //     }
-    //   }
-    // }
-    // console.log(jancode_list)
 })
 
 Quagga.onProcessed(data => {
@@ -96,7 +54,6 @@ Quagga.onProcessed(data => {
     return; 
   }
 
-  // 認識したバーコードを囲む
   if (data.boxes) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -105,12 +62,10 @@ Quagga.onProcessed(data => {
       Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, ctx, { color: 'green', lineWidth: 2 });
     });
 
-     // 読み取ったバーコードを囲む
-     if (data.box) {  
+    if (data.box) {  
       Quagga.ImageDebug.drawPath(data.box, { x: 0, y: 1 }, ctx, { color: 'blue', lineWidth: 2 });
     }
 
-    // 読み取ったバーコードに線を引く
     if (data.codeResult && data.codeResult.code) {
       Quagga.ImageDebug.drawPath(data.line, { x: 'x', y: 'y' }, ctx, { color: 'red', lineWidth: 3 });
     }
